7fa222416a81f4b4aeb903410556b577d5cd2761
guilhermefrag <guilhermefragnani@icloud.com>
2022-11-03 20:56:51 -0300

    subindo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import models.Usuario;
+
 public class App {
+
+    public static void Transacoes(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o código do usuário: ");
+        int codigo = scanner.nextInt();
+        // Usuario usuario = UsuarioDAO.getUsuarioByCodigo(codigo);
+        // if(usuario != null){
+        //     System.out.println("");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("--------------Bem Vindo--------------");
+        System.out.println("----------Escolha uma opção----------");
+        System.out.println("1 - Cadastrar Usuário");
+        System.out.println("2 - Entrar com Usuário");
     }
 }
