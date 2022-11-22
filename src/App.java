f2a7ef2c4ab48662647e5316bf072ce50908a1e6
Guilherme Fragnani <guilhermefragnani@icloud.com>
2022-11-22 12:37:39 -0300

    subindo

--- src/App.java
+++ src/App.java
@@ -11,116 +11,119 @@ import java.util.Scanner;
 
 public class App {
 
-  private static Usuario usuario = new Usuario();
-  private static Conta conta = new Conta();
-  private static Movimentacao movimentacao = new Movimentacao();
-  private static Connection conexao;
-  private static UsuarioDAO usuarioDAO;
-  private static ContaDAO contaDAO;
-  private static MovimentacaoDAO movimentacaoDAO;
-  private static ConnectionFactory connection = new ConnectionFactory();
-  private static Scanner scanner = new Scanner(System.in);
+    private static Usuario usuario = new Usuario();
+    private static Conta conta = new Conta();
+    private static Movimentacao movimentacao = new Movimentacao();
+    private static Connection conexao;
+    private static UsuarioDAO usuarioDAO;
+    private static ContaDAO contaDAO;
+    private static MovimentacaoDAO movimentacaoDAO;
+    private static ConnectionFactory connection = new ConnectionFactory();
+    private static Scanner scanner = new Scanner(System.in);
 
-  public static void main(String[] args) throws Exception {
-    ConnectionFactory connection = new ConnectionFactory();
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory connection = new ConnectionFactory();
 
-    conexao = connection.getConexao();
+        conexao = connection.getConexao();
 
-    System.out.println("\n--------------Bem Vindo--------------");
-    System.out.println("----------Escolha uma opção----------\n");
-    System.out.println("1 - Cadastrar Usuário");
-    System.out.println("2 - Entrar com Usuário\n");
-    System.out.println("Digite a opção: ");
-    int opcao = scanner.nextInt();
-    contaDAO = new ContaDAO(conexao);
-    movimentacaoDAO = new MovimentacaoDAO(conexao);
+        System.out.println("\n--------------Bem Vindo--------------");
+        System.out.println("----------Escolha uma opção----------\n");
+        System.out.println("1 - Cadastrar Usuário");
+        System.out.println("2 - Entrar com Usuário\n");
+        System.out.println("Digite a opção: ");
+        int opcao = scanner.nextInt();
+        contaDAO = new ContaDAO(conexao);
+        movimentacaoDAO = new MovimentacaoDAO(conexao);
 
-    if (opcao == 1) {
-      System.out.println("Digite o codigo do usuário: ");
-      usuario.setCodigo(scanner.nextInt());
-      System.out.println("Digite o nome do usuário:");
-      usuario.setNome(scanner.next());
-      System.out.println("Digite o sobrenome do usuário:");
-      usuario.setSobrenome(scanner.next());
-      System.out.println("Digite o cpf do usuário:");
-      usuario.setCpf(scanner.next());
-      System.out.println("Digite a senha do usuário:");
-      usuario.setSenha(scanner.next());
-      usuarioDAO = new UsuarioDAO(conexao);
-      usuarioDAO.Insert(usuario);
-      usuario.setId(usuarioDAO.SelectIdByNome(usuario.getNome()));
-      conta.setIdUsuario(usuario.getId());
-      conta.setValorTotal(0);
-      contaDAO.Insert(conta);
-    } else if (opcao == 2) {
-      System.out.println("Digite o seu nome: ");
-      usuario.setNome(scanner.next());
-      System.out.println("Digite sua senha: ");
-      usuario.setSenha(scanner.next());
+        if (opcao == 1) {
+            System.out.println("Digite o codigo do usuário: ");
+            usuario.setCodigo(scanner.nextInt());
+            System.out.println("Digite o nome do usuário:");
+            usuario.setNome(scanner.next());
+            System.out.println("Digite o sobrenome do usuário:");
+            usuario.setSobrenome(scanner.next());
+            System.out.println("Digite o cpf do usuário:");
+            usuario.setCpf(scanner.next());
+            System.out.println("Digite a senha do usuário:");
+            usuario.setSenha(scanner.next());
+            usuarioDAO = new UsuarioDAO(conexao);
+            usuarioDAO.Insert(usuario);
+            usuario.setId(usuarioDAO.SelectIdByNome(usuario.getNome()));
+            conta.setIdUsuario(usuario.getId());
+            conta.setValorTotal(0);
+            contaDAO.Insert(conta);
+        } else if (opcao == 2) {
+            System.out.println("Digite o seu nome: ");
+            usuario.setNome(scanner.next());
+            System.out.println("Digite sua senha: ");
+            usuario.setSenha(scanner.next());
 
-      usuarioDAO = new UsuarioDAO(conexao);
-      if (!usuarioDAO.Login(usuario.getNome(), usuario.getSenha())) {
-        System.out.println("Usuário ou senha incorretos!");
-        return;
-      }
-      System.out.println("Login efetuado com sucesso!");
-      usuario.setId(usuarioDAO.SelectIdByNome(usuario.getNome()));
-      conta.setIdUsuario(usuario.getId());
-      conta = contaDAO.SelectComClausula(usuario.getId());
-      Transacoes();
+            usuarioDAO = new UsuarioDAO(conexao);
+            if (!usuarioDAO.Login(usuario.getNome(), usuario.getSenha())) {
+                System.out.println("Usuário ou senha incorretos!");
+                return;
+            }
+            System.out.println("Login efetuado com sucesso!");
+            usuario.setId(usuarioDAO.SelectIdByNome(usuario.getNome()));
+            conta.setIdUsuario(usuario.getId());
+            conta = contaDAO.SelectComClausula(usuario.getId());
+            Transacoes();
+        }
     }
-  }
 
-  public static void Transacoes() throws SQLException {
-    System.out.println("-----Escolha a operação você deseja realizar-----\n");
-    System.out.println("1 - Saque\n");
-    System.out.println("2 - Depósito\n");
-    System.out.println("3 - Sair\n");
-    System.out.println("Digite a opção: ");
-    int opcao = scanner.nextInt();
-    if (opcao == 1) {
-      System.out.println("Digite o valor do saque: ");
-      double valor = scanner.nextDouble();
+    public static void Transacoes() throws SQLException {
+        System.out.println("-----Escolha a operação você deseja realizar-----\n");
+        System.out.println("1 - Saque\n");
+        System.out.println("2 - Depósito\n");
+        System.out.println("3 - Consultar Saldo\n");
+        System.out.println("4 - Sair\n");
+        System.out.println("Digite a opção: ");
+        int opcao = scanner.nextInt();
+        if (opcao == 1) {
+            System.out.println("Digite o valor do saque: ");
+            double valor = scanner.nextDouble();
 
-      conta.setValorTotal(
-        contaDAO.SelectComClausula(usuario.getId()).getValorTotal()
-      );
+            conta.setValorTotal(
+                    contaDAO.SelectComClausula(usuario.getId()).getValorTotal());
 
-      if (conta.getValorTotal() <= 0 || conta.getValorTotal() < valor) {
-        System.out.println("Sem valor disponível para esse saque");
-      } else {
-        conta.setValorTotal(conta.getValorTotal() - valor);
-        movimentacao.setIdUsuario(usuario.getId());
-        movimentacao.setTipo("Saque");
-        movimentacao.setValor(valor);
+            if (conta.getValorTotal() <= 0 || conta.getValorTotal() < valor) {
+                System.out.println("Sem valor disponível para esse saque");
+            } else {
+                conta.setValorTotal(conta.getValorTotal() - valor);
+                movimentacao.setIdUsuario(usuario.getId());
+                movimentacao.setTipo("Saque");
+                movimentacao.setValor(valor);
 
-        contaDAO.Update(conta);
-        movimentacaoDAO.Insert(movimentacao);
+                contaDAO.Update(conta);
+                movimentacaoDAO.Insert(movimentacao);
 
-        System.out.println("Valor sacado com sucesso!");
-        Transacoes();
-      }
-    } else if (opcao == 2) {
-      System.out.println("Digite o valor do depoósito: ");
-      double valor = scanner.nextDouble();
+                System.out.println("Valor sacado com sucesso!");
+                Transacoes();
+            }
+        } else if (opcao == 2) {
+            System.out.println("Digite o valor do depoósito: ");
+            double valor = scanner.nextDouble();
 
-      conta.setValorTotal(
-        contaDAO.SelectComClausula(usuario.getId()).getValorTotal()
-      );
+            conta.setValorTotal(
+                    contaDAO.SelectComClausula(usuario.getId()).getValorTotal());
 
-      conta.setValorTotal(conta.getValorTotal() + valor);
-      movimentacao.setIdUsuario(usuario.getId());
-      movimentacao.setTipo("Deposito");
-      movimentacao.setValor(valor);
+            conta.setValorTotal(conta.getValorTotal() + valor);
+            movimentacao.setIdUsuario(usuario.getId());
+            movimentacao.setTipo("Deposito");
+            movimentacao.setValor(valor);
 
-      contaDAO.Update(conta);
-      movimentacaoDAO.Insert(movimentacao);
+            contaDAO.Update(conta);
+            movimentacaoDAO.Insert(movimentacao);
 
-      System.out.println("Valor depositado com sucesso!");
-      Transacoes();
-    } else {
-      System.out.println("Saindo...");
-      return;
+            System.out.println("Valor depositado com sucesso!");
+            Transacoes();
+        }
+        else if (opcao == 3){
+            System.out.println("Seu saldo é: " + contaDAO.getSaldo(usuario.getId()));
+        }
+        else {
+            System.out.println("Saindo...");
+            return;
+        }
     }
-  }
 }
--- src/database/DAOs/ContaDAO.java
+++ src/database/DAOs/ContaDAO.java
@@ -10,63 +10,76 @@ import java.util.List;
 
 public class ContaDAO {
 
-  private Connection conexao;
-  private String selectComClausula = "SELECT * from conta WHERE id_usuario = ?";
-  private String insert =
-    "INSERT into conta " + "(id_usuario, valor_total) VALUES (?, ?)";
-  private String update =
-    "UPDATE conta " + "SET valor_total = ? WHERE id_usuario = ?";
+    private Connection conexao;
+    private String selectComClausula = "SELECT * from conta WHERE id_usuario = ?";
+    private String insert = "INSERT into conta " + "(id_usuario, valor_total) VALUES (?, ?)";
+    private String update = "UPDATE conta " + "SET valor_total = ? WHERE id_usuario = ?";
+    private String getSaldo = "SELECT valor_total from conta WHERE id_usuario = ?";
 
-  private PreparedStatement pstSelectComClasula;
-  private PreparedStatement pstInsert;
-  private PreparedStatement pstUpdate;
+    private PreparedStatement pstSelectComClasula;
+    private PreparedStatement pstInsert;
+    private PreparedStatement pstUpdate;
+    private PreparedStatement pstGetSaldo;
 
-  public ContaDAO(Connection conexao) throws SQLException {
-    this.conexao = conexao;
-    pstSelectComClasula = this.conexao.prepareStatement(selectComClausula);
-    pstInsert = this.conexao.prepareStatement(insert);
-    pstUpdate = this.conexao.prepareStatement(update);
-  }
-
-  public Conta SelectComClausula(int idUsuario) throws SQLException {
-    pstSelectComClasula.setInt(1, idUsuario);
-
-    ResultSet resultado = pstSelectComClasula.executeQuery();
-    Conta c = new Conta();
-
-    while (resultado.next()) {
-      c.setId(resultado.getInt("id"));
-      c.setIdUsuario(resultado.getInt("id_usuario"));
-      c.setValorTotal(resultado.getFloat("valor_total"));
-    }
-    return c;
-  }
-
-  public int SelectIdByNome(String nome) throws SQLException {
-    pstSelectComClasula.setString(1, nome);
-    ResultSet resultado = pstSelectComClasula.executeQuery();
-    int id = 0;
-
-    while (resultado.next()) {
-      id = resultado.getInt("id");
+    public ContaDAO(Connection conexao) throws SQLException {
+        this.conexao = conexao;
+        pstSelectComClasula = this.conexao.prepareStatement(selectComClausula);
+        pstInsert = this.conexao.prepareStatement(insert);
+        pstUpdate = this.conexao.prepareStatement(update);
+        pstGetSaldo = this.conexao.prepareStatement(getSaldo);
     }
 
-    return id;
-  }
+    public Conta SelectComClausula(int idUsuario) throws SQLException {
+        pstSelectComClasula.setInt(1, idUsuario);
 
-  public int Insert(Object param) throws SQLException {
-    Conta p = (Conta) param;
+        ResultSet resultado = pstSelectComClasula.executeQuery();
+        Conta c = new Conta();
 
-    pstInsert.setInt(1, p.getIdUsuario());
-    pstInsert.setDouble(2, p.getValorTotal());
-    return pstInsert.executeUpdate();
-  }
+        while (resultado.next()) {
+            c.setId(resultado.getInt("id"));
+            c.setIdUsuario(resultado.getInt("id_usuario"));
+            c.setValorTotal(resultado.getFloat("valor_total"));
+        }
+        return c;
+    }
 
-  public int Update(Object param) throws SQLException {
-    Conta p = (Conta) param;
+    public double getSaldo(int idUsuario) throws SQLException {
+        pstGetSaldo.setInt(1, idUsuario);
 
-    pstUpdate.setDouble(1, p.getValorTotal());
-    pstUpdate.setInt(2, p.getIdUsuario());
-    return pstUpdate.executeUpdate();
-  }
+        ResultSet resultado = pstGetSaldo.executeQuery();
+        double saldo = 0;
+
+        while (resultado.next()) {
+            saldo = resultado.getDouble("valor_total");
+        }
+        return saldo;
+    }
+
+    public int SelectIdByNome(String nome) throws SQLException {
+        pstSelectComClasula.setString(1, nome);
+        ResultSet resultado = pstSelectComClasula.executeQuery();
+        int id = 0;
+
+        while (resultado.next()) {
+            id = resultado.getInt("id");
+        }
+
+        return id;
+    }
+
+    public int Insert(Object param) throws SQLException {
+        Conta p = (Conta) param;
+
+        pstInsert.setInt(1, p.getIdUsuario());
+        pstInsert.setDouble(2, p.getValorTotal());
+        return pstInsert.executeUpdate();
+    }
+
+    public int Update(Object param) throws SQLException {
+        Conta p = (Conta) param;
+
+        pstUpdate.setDouble(1, p.getValorTotal());
+        pstUpdate.setInt(2, p.getIdUsuario());
+        return pstUpdate.executeUpdate();
+    }
 }
